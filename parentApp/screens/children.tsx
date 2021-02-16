6dcb1921eafa72dfc14a179924c3c724be2c3336
TheMole3 <hugomelo04@gmail.com>
2021-02-16 13:12:22 +0100

    Update children.tsx

--- parentApp/screens/children.tsx
+++ parentApp/screens/children.tsx
@@ -6,22 +6,52 @@ import globalStyle from "../../stylesheets/globalStyle"
 import SafeViewAndroid from "../../components/SafeViewAndroid";
 import Background from "../../components/parentBackground";
 import SvgUri from 'expo-svg-uri';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function childrenScreen({navigation}:any) {
     return (
         <View style={{flex:1}}>
         <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea]}>
           <View style={globalStyle.container}>
-            <View>
-                <View>
-                    <SvgUri>
-                        
-                    </SvgUri>
-                </View>
+            <View style={{
+                position: "absolute",
+                display: "flex",
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-evenly",
+                width: "80%",
+            }}>
+                <TouchableOpacity style={profileContainerStyle}>
+                    <SvgUri
+                        source={require('../../assets/emoji/cactus.svg')}
+                    />
+                    <Text style={profileText}>Adam</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={profileContainerStyle}>
+                    <SvgUri
+                        source={require('../../assets/emoji/penguin.svg')}
+                    />
+                    <Text style={profileText}>Hugo</Text>
+                </TouchableOpacity>
             </View>
           </View>
         </SafeAreaView>
         <Background></Background>
       </View>
     );
+}
+
+var profileContainerStyle = {
+    width: 120,
+    height: 120,
+    backgroundColor: "#f6f7c3CC",
+    borderRadius: 50,
+    padding: 10,
+    paddingBottom: 40,
+}
+var profileText = {
+    fontFamily: "Oswald-Regular",
+    fontSize: 20,
+    alignSelf: "center",
+    bottom: 1,
 }
