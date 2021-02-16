0cff6754f9cdb0ee098363af1e400b4ff3d8aede
TheMole3 <hugomelo04@gmail.com>
2021-02-16 08:56:47 +0100

    Fixed camera buttons and added parent screens

--- parentApp/parentView.tsx
+++ parentApp/parentView.tsx
@@ -1,13 +1,76 @@
+global.pId = 1
+
 import locale from "../language/sv_SE.json"
 
 import * as React from 'react';
 import { Text, View, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function() {
-    return (
-        <Text>No</Text>
-    )
+var UnimplementedScreen = function() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>{locale.error.notImplemented}</Text>
+    </View>
+  );
+}
+
+const TaskStack = createStackNavigator();
+
+var ChildrenScreen = require("./screens/children.tsx").default;
+var CreateRewardScreen = require("./screens/createTask.tsx").default;
+var CreateTaskScreen = require("./screens/createRewardScreen.tsx").default;
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+      <Tab.Navigator>
+        <Tab.Screen name="children" 
+          options={{
+            title: locale.pages.yourChildren, 
+            tabBarIcon: ({focused, color, size}) => (
+              <Image
+                source={require('../assets/tasks.png')}
+                style={{
+                  width: size,
+                  height: size,
+                }}
+              />
+            ),
+          }} 
+          component={ChildrenScreen} 
+        />
+        <Tab.Screen name="createReward"
+            options={{
+            title: locale.pages.createReward, 
+            tabBarIcon: ({focused, color, size}) => (
+                <Image
+                source={require('../assets/tasks.png')}
+                style={{
+                    width: size,
+                    height: size,
+                }}
+                />
+            ),
+            }} 
+            component={CreateRewardScreen}
+        />
+        <Tab.Screen name="createTask"
+            options={{
+            title: locale.pages.createTask, 
+            tabBarIcon: ({focused, color, size}) => (
+                <Image
+                source={require('../assets/tasks.png')}
+                style={{
+                    width: size,
+                    height: size,
+                }}
+                />
+            ),
+            }} 
+            component={CreateTaskScreen}
+        />
+      </Tab.Navigator>
+  );
 }
