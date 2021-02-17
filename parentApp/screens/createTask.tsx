0c96f08bbd73b7cf2c6ba918944d01a7e0e33fb9
TheMole3 <hugomelo04@gmail.com>
2021-02-17 14:27:09 +0100

    Update createTask.tsx

--- parentApp/screens/createTask.tsx
+++ parentApp/screens/createTask.tsx
@@ -5,13 +5,13 @@ import {LinearGradient} from 'expo-linear-gradient';
 import globalStyle from "../../stylesheets/globalStyle"
 
 import SafeViewAndroid from "../../components/SafeViewAndroid";
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import SvgUri from 'expo-svg-uri';
 
 export default class createTask extends Component{ 
     constructor(props){ 
         super(props) 
-        this.state = { taskName:'',taskPoints:'', child1: false, child2: false} 
+        this.state = { taskName:'',taskPoints:'', child1: false, child2: false, selectedIcon: "1F3AB"} 
         this.handleChange = this.handleChange.bind(this) 
       } 
       
@@ -29,93 +29,117 @@ export default class createTask extends Component{
 
 
     render(){ 
+    var emojis = require("../../assets/taskImages/taskImages").default;
+    const emojiList = Object.keys(emojis).map((data) => {
+        return (
+            <TouchableOpacity style={{
+                backgroundColor: "#ae69f5cc"
+            }}
+            onPress={() => {this.setState({selectedIcon: data})}}
+            >
+                <SvgUri style={[{width:80, height:80}, data==this.state.selectedIcon ? {borderWidth:5}:{borderWidth:0}]} source={emojis[data]}/>
+            </TouchableOpacity>
+        )
+    })
+
     return (
         <View style={{flex:1}}>
         <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea]}>
-          <View style={[globalStyle.container, {
-            justifyContent: "center",
-            paddingBottom: 60
-          }]}>
-            <View style={{paddingTop: 180}}>
-                <Text>Uppgiftens namn</Text>
-                <TextInput
-                    style={{ height: 40, width: 200,borderColor: 'gray', borderWidth: 1, borderRadius: 5, padding: 10, backgroundColor:"#f6f7c3CC"}}
-                    value = {this.state.taskName} 
-                    onChange={e => this.handleChange(e,'taskName')}
-                />
-            </View>
-            <View style={{alignContent: "flex-start", paddingTop: 20}}>
-                <Text>Antal poäng</Text>
-                <TextInput
-                    keyboardType="numeric"
-                    style={{ height: 40, width: 200,borderColor: 'gray', borderWidth: 1, borderRadius: 5, padding: 10, backgroundColor:"#f6f7c3CC"}}
-                    value = {this.state.taskPoints} 
-                    onChange={e => this.handleChange(e,'taskPoints')}
-                />
-            </View>
-
             <View style={{
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-                width: "80%",
-                paddingTop: 40
+                flexDirection: "row"
+
             }}>
-                <TouchableOpacity 
-                    style={[style.profileContainerStyle, this.state.child1 ? {
-                        borderWidth: 2,
-                        borderColor: "red"
-                    }:{
-                        borderWidth: 0
-                    }]}
-                    onPress={() => {this.setState({child1: !this.state.child1})}}
+                <ScrollView
+                horizontal={true}
                 >
-                    <SvgUri
-                        source={require('../../assets/emoji/cactus.svg')}
-                    />
-                    <Text style={style.profileText}>Adam</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={[style.profileContainerStyle, this.state.child2 ? {
-                        borderWidth: 2,
-                        borderColor: "red"
-                    }:{
-                        borderWidth: 0
-                    }]}
-                    onPress={() => {this.setState({child2: !this.state.child2})}}
-                    >
-                    <SvgUri
-                        source={require('../../assets/emoji/penguin.svg')}
-                    />
-                    <Text style={style.profileText}>Hugo</Text>
-                </TouchableOpacity>
+                    {emojiList}
+                </ScrollView>
             </View>
 
-          <View style={{
-            position: 'absolute',
-            right: 10,
-            bottom: 20
-          }}>
-            <TouchableOpacity style={{
-              backgroundColor: "#f6f7c3CC",
-              borderRadius: 5,
-              padding: 20,
-            }}>
-              <Text style={{
-        fontFamily: "Oswald-Regular",
-        fontSize: 20,              
-        }}>Skapa Uppgift</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        </SafeAreaView>
+            <View style={[globalStyle.container, {
+                justifyContent: "center",
+                paddingBottom: 60
+              }]}>
+                <View style={{alignContent: "flex-start", paddingTop: 20}}>
+                    <Text>Uppgiftens namn</Text>
+                    <TextInput
+                        style={{ height: 40, width: 200,borderColor: 'gray', borderWidth: 1, borderRadius: 5, padding: 10, backgroundColor:"#f6f7c3CC"}}
+                        value = {this.state.taskName} 
+                        onChange={e => this.handleChange(e,'taskName')}
+                    />
+                </View>
+                <View style={{alignContent: "flex-start"}}>
+                    <Text>Antal poäng</Text>
+                    <TextInput
+                        keyboardType="numeric"
+                        style={{ height: 40, width: 200,borderColor: 'gray', borderWidth: 1, borderRadius: 5, padding: 10, backgroundColor:"#f6f7c3CC"}}
+                        value = {this.state.taskPoints} 
+                        onChange={e => this.handleChange(e,'taskPoints')}
+                    />
+                </View>
 
-        <LinearGradient
-            colors={['#7400B8', "#6930C3", "#5E60CE", "#5390D9", "#4EA8DE", "#48BFE3", "#56CFE1", "#64DFDF", "#72EFDD", "#80FFDB"]}
-            style={globalStyle.linearGradient}
-            start={{ x: .4, y: 0 }}
-            end={{ x: .9, y: 1 }}
-            locations={[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]}
-        ></LinearGradient>
-      </View>
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "space-evenly",
+                    width: "80%",
+                    paddingTop: 40
+                }}>
+                    <TouchableOpacity 
+                        style={[style.profileContainerStyle, this.state.child1 ? {
+                            borderWidth: 2,
+                            borderColor: "red"
+                        }:{
+                            borderWidth: 0
+                        }]}
+                        onPress={() => {this.setState({child1: !this.state.child1})}}
+                    >
+                        <SvgUri
+                            source={require('../../assets/emoji/cactus.svg')}
+                        />
+                        <Text style={style.profileText}>Adam</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[style.profileContainerStyle, this.state.child2 ? {
+                            borderWidth: 2,
+                            borderColor: "red"
+                        }:{
+                            borderWidth: 0
+                        }]}
+                        onPress={() => {this.setState({child2: !this.state.child2})}}
+                        >
+                        <SvgUri
+                            source={require('../../assets/emoji/penguin.svg')}
+                        />
+                        <Text style={style.profileText}>Hugo</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{
+                    position: 'absolute',
+                    right: 10,
+                    bottom: 20
+                }}>
+                    <TouchableOpacity style={{
+                    backgroundColor: "#f6f7c3CC",
+                    borderRadius: 5,
+                    padding: 20,
+                    }}>
+                        <Text style={{
+                        fontFamily: "Oswald-Regular",
+                        fontSize: 20,              
+                        }}>Skapa Uppgift</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            </SafeAreaView>
+
+
+            <LinearGradient
+                colors={['#7400B8', "#6930C3", "#5E60CE", "#5390D9", "#4EA8DE", "#48BFE3", "#56CFE1", "#64DFDF", "#72EFDD", "#80FFDB"]}
+                style={globalStyle.linearGradient}
+                start={{ x: .4, y: 0 }}
+                end={{ x: .9, y: 1 }}
+                locations={[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]}
+            ></LinearGradient>
+        </View>
     );
     }
 }
